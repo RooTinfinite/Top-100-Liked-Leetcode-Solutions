386eded2b0fe5ca3bb4eadd83d15232ca8fb7621
Piotr M <filo74b@gmail.com>
2024-10-27 23:39:44 +0100

    Time: 7 ms (86.01%) | Memory: 16.8 MB (24.97%) - LeetSync

--- Solutions/39-combination-sum/combination-sum.py
+++ Solutions/39-combination-sum/combination-sum.py
@@ -1,21 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         def backtrack(start, path, target):
-            # If the target is zero, we've found a valid combination
             if target == 0:
-                fResult.append(path[:])  # Add a copy of the current path
+                fResult.append(path[:])
                 return
-            # Iterate through candidates, starting from the current index
             for i in range(start, len(candidates)):
                 candidate = candidates[i]
                 if candidate <= target:
-                    # Choose the candidate and backtrack with reduced target
                     path.append(candidate)
-                    backtrack(i, path, target - candidate)  # Allow the same candidate again
-                    # Backtrack step (remove the last chosen candidate)
+                    backtrack(i, path, target - candidate)
                     path.pop()
-
         fResult = []
-        candidates.sort()  # Sorting helps in pruning, though it's optional
+        candidates.sort()
         backtrack(0, [], target)
         return fResult
