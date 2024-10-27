5ad131c2780fe5c856d9b87c67f477bcb8b85113
Piotr M <filo74b@gmail.com>
2024-10-28 00:50:28 +0100

    Time: 1 ms (93.46%) | Memory: 44.3 MB (81.20%) - LeetSync

--- Solutions/46-permutations/permutations.java
+++ Solutions/46-permutations/permutations.java
@@ -1,33 +1,35 @@
 class Solution {
-    // Helper function that generates all permutations using backtracking
-    private void solve(int[] nums, int l, int r, List<List<Integer>> result) {
-        if (l == r) {
-            List<Integer> temp = new ArrayList<>();
-            for (int num : nums) {
-                temp.add(num);
-            }
-            result.add(temp);
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, res);
+        return res;    
+    }
+
+    private void backtrack(int[] nums, int start, List<List<Integer>> res) {
+        if (start == nums.length) {
+            res.add(arrayToList(nums));
             return;
         }
-        
-        for (int i = l; i <= r; i++) {
-            swap(nums, l, i);
-            solve(nums, l + 1, r, result);
-            swap(nums, l, i);
+
+        for (int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            backtrack(nums, start + 1, res);
+            swap(nums, start, i);
         }
     }
     
-    // Helper function to swap elements in array
+    private List<Integer> arrayToList(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : arr) {
+            list.add(num);
+        }
+        return list;
+    }
+    
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-    }
-    
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        solve(nums, 0, nums.length - 1, result);
-        return result;
+        
     }
 }
-
