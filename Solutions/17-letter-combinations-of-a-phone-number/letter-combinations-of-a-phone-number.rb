52da61672759427f43ba2835458284cf6bf475b8
Piotr M <filo74b@gmail.com>
2024-10-27 21:10:38 +0100

    Time: 0 ms (100.00%) | Memory: 211.6 MB (15.24%) - LeetSync

--- Solutions/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.rb
+++ Solutions/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.rb
@@ -1,29 +1,30 @@
 def letter_combinations(digits)
-    return [] if digits.empty?
-    
-    phone_map = {
-        '2' => 'abc',
-        '3' => 'def',
-        '4' => 'ghi',
-        '5' => 'jkl',
-        '6' => 'mno',
-        '7' => 'pqrs',
-        '8' => 'tuv',
-        '9' => 'wxyz'
+    @res = []
+
+    @digits = digits
+    @digitToChar = {
+        "2" => "abc",
+        "3" => "def",
+        "4" => "ghi",
+        "5" => "jkl",
+        "6" => "mno",
+        "7" => "qprs",
+        "8" => "tuv",
+        "9" => "wxyz",
     }
-    
-    @output = []
-    
-    def backtrack(combination, next_digits, phone_map)
-        if next_digits.empty?
-            @output << combination
-        else
-            phone_map[next_digits[0]].each_char do |letter|
-                backtrack(combination + letter, next_digits[1..-1], phone_map)
-            end
+
+    def backtrack(i, curStr)
+        if curStr.length == @digits.length
+            @res.push(curStr)
+            return
+        end
+
+        for c in @digitToChar[@digits[i]].chars
+            backtrack(i + 1, curStr + c)
         end
     end
-    
-    backtrack("", digits, phone_map)
-    @output
+
+    backtrack(0, "") if @digits && @digits != ""
+
+    @res
 end
