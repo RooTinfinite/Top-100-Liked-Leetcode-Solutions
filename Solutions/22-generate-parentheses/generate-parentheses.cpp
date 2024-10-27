21ecc006f32da6b43bb25de0806eb5271a525204
Piotr M <filo74b@gmail.com>
2024-10-27 22:43:02 +0100

    Time: 0 ms (100.00%) | Memory: 12.1 MB (83.95%) - LeetSync

--- Solutions/22-generate-parentheses/generate-parentheses.cpp
+++ Solutions/22-generate-parentheses/generate-parentheses.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
-    void solve(string op, int open, int close, vector<string> &ans){
-        if(open == 0 && close == 0){
-            ans.push_back(op);
-            return;
-        }
-        //when count of open and close brackets are same then 
-        //we have only one choice to put open bracket 
-        if(open == close){
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else if(open == 0){
-            //only choice is to put close brackets 
-            string op1 = op;
-            op1.push_back(')');
-            solve(op1, open, close-1, ans);
-        }
-        else if(close == 0){
-            //only choise is to use open bracket 
-            string op1 = op;
-            op1.push_back('(');
-            solve(op1, open-1, close, ans);
-        }
-        else{
-            string op1 = op;
-            string op2 = op;
-            op1.push_back('(');
-            op2.push_back(')');
-            solve(op1, open-1, close, ans);
-            solve(op2, open, close-1, ans);
-        }
+   void solve(vector<string>& ans, string& s, int open, int close, int n) {
+    if (close == n && open == n) {
+        ans.push_back(s);
+        return;
     }
-    vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
-        vector<string> ans;
-        string op = "";
-        solve(op, open, close, ans);
-        return ans;
+    if (open < n) {
+        s += "(";
+        solve(ans, s, open + 1, close, n);
+        s.pop_back(); // Backtrack
     }
+    if (close < open) {
+        s += ")";
+        solve(ans, s, open, close + 1, n);
+        s.pop_back(); // Backtrack
+    }
+}
+
+vector<string> generateParenthesis(int n) {
+    vector<string> ans;
+    string s;
+    solve(ans, s, 0, 0, n);
+    return ans;
+}
 };
