cc1b0fdf218b980b40cf56c763159c090ad7df41
Piotr M <filo74b@gmail.com>
2024-10-27 23:17:47 +0100

    Time: 0 ms (100.00%) | Memory: 18.3 MB (5.83%) - LeetSync

--- Solutions/22-generate-parentheses/generate-parentheses.swift
+++ Solutions/22-generate-parentheses/generate-parentheses.swift
@@ -1,43 +1,28 @@
 class Solution {
-    // Function that generates all valid combinations of n pairs of parentheses
     func generateParenthesis(_ n: Int) -> [String] {
-        // Stack to build current combination
         var stack: [String] = []
-        // Array to store all valid combinations
         var res: [String] = []
-        
-        // Recursive backtracking function
-        // open: count of opening brackets used
-        // close: count of closing brackets used
+
         func backtrack(_ open: Int, _ close: Int) {
-            // Base case: when we have used n opening and n closing brackets
-            // we have a valid combination
             if open == close && close == n {
-                // Join all characters in stack and add to result
                 res.append(stack.joined())
                 return
             }
-            
-            // If we haven't used all opening brackets yet
-            // we can add an opening bracket
+
             if open < n {
-                stack.append("(")                // Add opening bracket
-                backtrack(open + 1, close)       // Recurse with one more opening bracket
-                stack.removeLast()               // Backtrack by removing the bracket
+                stack.append("(")
+                backtrack(open + 1, close)
+                stack.removeLast()
             }
-            
-            // If we have more opening brackets than closing brackets
-            // we can add a closing bracket
+
             if close < open {
-                stack.append(")")                // Add closing bracket
-                backtrack(open, close + 1)       // Recurse with one more closing bracket
-                stack.removeLast()               // Backtrack by removing the bracket
+                stack.append(")")
+                backtrack(open, close + 1)
+                stack.removeLast()
             }
         }
-        
-        // Start the recursion with 0 opening and closing brackets
+
         backtrack(0, 0)
         return res
     }
 }
-
