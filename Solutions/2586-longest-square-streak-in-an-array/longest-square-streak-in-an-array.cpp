ed8f3cc59710e2a138faccbc711e5c150439e7a9
Piotr M <filo74b@gmail.com>
2024-10-28 01:18:20 +0100

    Time: 16 ms (95.56%) | Memory: 85.7 MB (99.36%) - LeetSync

--- Solutions/2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ Solutions/2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,24 +1,30 @@
-class Solution 
-{
+class Solution {
 public:
-    int longestSquareStreak(vector<int>& nums) 
-    {
-        auto isqrt = [](int n) { return (int)floor(sqrt(n)); };
-        
-        sort(nums.begin(), nums.end());
-        nums.erase(unique(nums.begin(), nums.end()), nums.end());
-        
-        int s, c = 0;
-        unordered_map<int,int> sqr;
-        
-        for (int n : nums)
-        {
-            sqr[n] = 1;
-            while ((s = isqrt(n)) && s*s == n && sqr.count(s))
-                sqr[s] += 1, n = s;
+    int longestSquareStreak(vector<int>& nums) {
+        int n = nums.size(), mx = 0;
+
+        int dp[100001];
+        memset(dp, 0, sizeof(dp));
+
+        for(int i = 0; i < n; i++){
+            dp[nums[i]] = 1;
+            mx = max(mx, nums[i]);
         }
-        
-        for (auto[n,s] : sqr) c = max(c,s);
-        return c > 1 ? c : -1;
+
+        int ans = -1;
+
+        for(int i = 2; i <= mx; i++){
+            if(dp[i] && (int)sqrt(i) * (int)sqrt(i) == i){
+                dp[i] = dp[i] + dp[(int)sqrt(i)];
+                ans = max(ans, dp[i]);
+                cout<<i<<endl;
+            }
+        }
+
+        if(ans == 1){
+            return -1;
+        }
+
+        return ans;
     }
 };
